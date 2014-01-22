214564250f1aa27e0aaf5f9a7c5c0fadf4d368ee
Mark <mark@a.b.c.d>
2014-01-22 16:41:54 +0800

    Add service, remove unnecessary bin files

---
+++ Testee/src/edu/sinica/testee/RandomService.java
@@ -0,0 +1,15 @@
+package edu.sinica.testee;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class RandomService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
