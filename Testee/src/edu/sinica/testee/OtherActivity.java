c8885b433ee1aa35933144145c627665c9e82124
Mark <mark@a.b.c.d>
2014-01-22 16:22:30 +0800

    Add OtherAcitivity, remove bin

---
+++ Testee/src/edu/sinica/testee/OtherActivity.java
@@ -0,0 +1,15 @@
+package edu.sinica.testee;
+
+import android.app.Activity;
+
+public class OtherActivity extends Activity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
